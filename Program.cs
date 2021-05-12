a7bef221223b74b4a7a18241e5951981fc697c93
Miriam410 <chenmiriam410@gmail.com>
2021-05-12 18:15:03 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero que se le va a sumar 50");
+            int ingreso = Convert.ToInt32(Console.ReadLine());
+            const int n= 50;
+            int suma = ingreso + n;
+            Console.WriteLine($"La suma da: {suma}");
+            Console.ReadLine();
+        }
+    }
+}
